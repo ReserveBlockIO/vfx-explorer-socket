55e0563bb51b8c2e288dcd066db51833acda8d83
Tyler Savery <tyler@theyoungastronauts.com>
2024-11-26 15:27:30 -0500

    cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const express = require('express');
+const cors = require('cors');
+
 
 const app = express();
 
@@ -9,7 +11,16 @@ const server = app.listen(port, () => {
     console.log(`listening on *:${port}`);
 });
 
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*", // Replace with your client URL
+        methods: ["GET", "POST"],
+        allowedHeaders: ["*"],
+        credentials: false
+    }
+});
+
+app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
